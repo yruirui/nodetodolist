8f68627ffab48d668c855ef9a011d004376deec7
ruirui <yruirui96@gmail.com>
2022-09-22 10:33:12 +0800

    测试两个例子运行报错

--- __tests__/__mocks__/fs.js
+++ __tests__/__mocks__/fs.js
@@ -5,18 +5,35 @@ const _fs=jest.requireActual('fs')
 
 Object.assign(fs,_fs)
 
-const mocks={}
+let mocks={}
 
 fs.setMock=(path,error,data)=>{
     mocks[path]=[error,data]
 }
-
+//这里对readfile做了一个拦截，当路径是在mocks里就走mock出的数据
+//不然就走原fs的方法，原fs是被拷贝在_fs里的
+//这里如果用户没有传callback时需要指定一下callback
 fs.readFile = (path,options,callback)=>{
     if(callback===undefined){callback=options}
     if(path in mocks){
         callback(mocks[path][0],mocks[path][1])
     }else{
         _fs.readFile(path,options,callback)
+    }
+}
+
+//写的操作
+
+let writeMock={}
+fs.setWriteFileMock=(path,fn)=>{
+    writeMock[path]=fn
+}
+fs.writeFile=(path,data,options,callback)=>{
+    if(callback === undefined){ callback = options}
+    if(path in writeMock){
+        writerMocks[path](path,data,options,callback)
+    }else{
+        _fs.writeFile(path,data,options,callback)
     }
 }
 
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -3,10 +3,20 @@ const fs=require('fs')
 jest.mock('fs')
 
 describe('db',()=>{
-    it('can read',()=>{
-       expect(db.read instanceof Function)
+    it('can read',async ()=>{
+       const data =[{title:'hi',done:true}]
+       fs.setMock('./xxx','null',JSON.stringify(data))
+       const list = await db.read('./xxx')
+       expect(list).toStrickEqual(data)
     })
-    it('can write',()=>{
-        expect(fs.x()).toBe('xxx')
+    it('can write',async ()=>{
+        let fakeFile
+        fs.setWriteFileMock('./yyy',(path,data,callback)=>{
+            fakeFile=data
+            callback(null)
+        })
+        const list2 = [{title:'a apple',done:true}]
+        await db.write(list2,'./yyy')
+        expect(fakeFile).toBe(JSON.stringify(list2)+'\n')
     })
 })
